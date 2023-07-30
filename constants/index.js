7c5bfd114ad7a72daad183361253d764ed015b5d
nobxmehrabi <130984071+nobxmehrabi@users.noreply.github.com>
2023-07-30 23:52:12 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc883577d51b2968ac1f23154484d69afbd4707d5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
